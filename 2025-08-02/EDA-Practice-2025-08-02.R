43c4d34d707dce67d22fe913e788d05950df903c
Alex Minsung Kim <alexminsung.kim@hotmail.com>
2025-08-05 22:15:04 -0400

    Finalize 2025-08-02 EDA project

--- 2025-08-02/EDA-Practice-2025-08-02.R
+++ 2025-08-02/EDA-Practice-2025-08-02.R
@@ -5,48 +5,71 @@ library(readr)
 # Load the data
 person <- read_csv("2025-08-02/person.zip")
 
-# Select columns related to motorcyclist fatalities and filter out all non two-wheeled vehicles
+# Categorize vehicles by their type
 person <- person %>% 
-  filter(grepl("Motorcycle", BODY_TYPNAME)) %>%
-  select(DOA, DRINKING, HELM_USE, INJ_SEV, BODY_TYPNAME, MONTHNAME)
- 
-# Replace DOA indicators from numbers to attributes
-# Case_when function allows for multiple `if_else()` statements as seen below
-person <- person %>% mutate(DOA = case_when(
-  DOA == 0 ~ "Not Applicable",
-  DOA ==  7 ~ "Died at Scene",
-  DOA == 8 ~ "Died En Route",
-  DOA == 9 ~ "Unknown"
+  # Case_when allows for multiple `if_else()` statements. 
+  mutate(type = case_when(
+    grepl("Motorcycle", VPICBODYCLASSNAME) ~ "Motorcycle", 
+    grepl("SUV|Hatchback|Sedan|Coupe|Convertible|cUV|Wagon|Roadster", VPICBODYCLASSNAME) ~ "Passenger Cars",
+    grepl("Pickup|SUT|Van|Motorhome|Minivan|Cutaway|Motor Home Chassis", VPICBODYCLASSNAME) ~ "Light Trucks and Vans",
+    grepl("Bus", VPICBODYCLASSNAME) ~ "Bus",
+    grepl("Truck|Fire Apparatus|Chassis Cab|Commercial Chassis", VPICBODYCLASSNAME) ~ "Medium and Heavy Trucks",
+    grepl("Other|Enduro|Golf Cart|Farm Equipment|Construction Equipment|Go Kart|Street Sweeper|LSV", VPICBODYCLASSNAME) ~ "Other",
+    .default = "Unknown or Incomplete"
+  ))
+person <- person %>% filter(type != "Unknown or Incomplete") %>% 
+  mutate(type = factor(type, levels = c("Passenger Cars", "Motorcycle", "Light Trucks and Vans", "Medium and Heavy Trucks", "Bus", "Other")))
+
+# Populate the vehiecle type dropdown options
+vehicle_type <- unique(person$type)
+
+# Leading cause of traffic fatalities in 2023
+# Obtain total count
+type_count <- person %>% group_by(type) %>% summarise(total_count = n())
+# Obtain total fatality count and calculate the relative frequency of people who died from a crash
+death_count <- person %>% filter(INJ_SEVNAME == "Fatal Injury (K)") %>% group_by(type) %>% 
+  summarise(fatality_count = n()) %>% left_join(type_count) %>% mutate(percentage = round(100*(fatality_count/total_count),2)) %>% arrange(type) 
+# Add a column of rank
+death_count <- death_count %>% mutate(rank = case_when(
+  type == "Passenger Cars" ~ 1,
+  type == "Motorcycle" ~ 2,
+  type == "Light Trucks and Vans" ~ 3,
+  type == "Medium and Heavy Trucks" ~ 4,
+  type == "Other" ~ 5,
+  type == "Bus" ~ 6
 ))
 
-# Replace DRINKING indicators to comprehensible attributes
-person <- person %>% mutate(DRINKING = case_when(
-  DRINKING == 0 ~ "No Alcohol",
-  DRINKING ==  1 ~ "Alcohol Involved",
-  DRINKING == 8 ~ "Not Reported",
-  DRINKING == 9 ~ "Unknown"
-))
+# Separate df for rank in `pivot_wide` form
+lead <- death_count %>% select(type, rank) %>% pivot_wider(names_from = type, values_from = rank)
 
-# Replace HELM_USE indicators to comprehensible attributes
-person <- person %>% mutate(HELM_USE = case_when(
-  HELM_USE == 5 ~ "DOT-Compliant Motorcycle Helmet",
-  HELM_USE ==  16 ~ "Helmet, Other than DOT-Compliant Motorcycle Helmet",
-  HELM_USE == 17 ~ "No Helmet",
-  HELM_USE == 19 ~ "Helmet, Unknown if DOT-Compliant ",
-  HELM_USE == 20 ~ "Not Applicable",
-  HELM_USE == 96 ~ "Not a Motor Vehicle Occupant",
-  HELM_USE == 98 ~ "Not Reported",
-  HELM_USE == 99 ~ "Unknown"
-))
+# Separate df for percentage in `pivot_wide` form
+percentage_death_by_vehicle <- death_count %>% select(type, percentage) %>% pivot_wider(names_from = type, values_from = percentage)
+clipr::write_clip(percentage_death_by_vehicle)
 
-# Replace INJ_SEV indicators to comprehensible attributes
-person <- person %>% mutate(INJ_SEV = case_when(
-  INJ_SEV == 0 ~ "No Apparent Injury",
-  INJ_SEV ==  1 ~ "Possible Injury",
-  INJ_SEV == 2 ~ "Suspected Minor Injury",
-  INJ_SEV == 3 ~ "Suspected Serious Injury",
-  INJ_SEV == 4 ~ "Fatal Injury",
-  INJ_SEV == 5 ~ "Injured, Severity Unknown",
-  INJ_SEV == 6 ~ "Died Prior to Crash",
-  INJ_SEV == 9 ~ "Unknown"
-))
+# Separate df containing information for pie-chart in `pivot_wide` form
+percentage_death <- death_count %>% mutate(percentage = case_when(
+  type == "Passenger Cars" ~ round(fatality_count[1]/sum(death_count$fatality_count),4)*100,
+  type == "Motorcycle" ~ round(fatality_count[2]/sum(death_count$fatality_count),4)*100,
+  type == "Light Trucks and Vans" ~ round(fatality_count[3]/sum(death_count$fatality_count),4)*100,
+  type == "Medium and Heavy Trucks" ~ round(fatality_count[4]/sum(death_count$fatality_count),4)*100,
+  type == "Bus" ~ round(fatality_count[5]/sum(death_count$fatality_count),4)*100,
+  type == "Other" ~ round(fatality_count[6]/sum(death_count$fatality_count),4)*100
+)) %>% select(type, percentage) %>% pivot_wider(names_from = type, values_from = percentage)
+# Add another row of "other" traffic fatalities other than the vehicle type in question
+percentage_death <- percentage_death %>% add_row(100 - percentage_death[1, ])
+# Rename the rows to give an apporpriate label
+row.names(percentage_death) <- c("Traffic Fatality(%)", "Other(%)")
+
+
+# Motorcycle fatalities based on month
+motorcycle_fatalities_by_month <- person %>% 
+  filter(type == "Motorcycle" & INJ_SEVNAME == "Fatal Injury (K)") %>%
+  group_by(INJ_SEVNAME, MONTHNAME) %>% summarise(fatality_count = n()) %>% ungroup() %>% select(-INJ_SEVNAME)
+# Rearrange by correct month order
+motorcycle_fatalities_by_month <- motorcycle_fatalities_by_month %>%
+  mutate(MONTHNAME = factor(MONTHNAME, levels = c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))) 
+
+
+# Credits: Info-graphic - https://www.youtube.com/watch?v=RXdcHCPJRg8
+# Credits: Data - NHTSA 2023 FARS data
+# Credits: Vehicle type classification based on https://www.fhwa.dot.gov/policyinformation/tmguide/tmg_2013/vehicle-types.cfm
---
+++ 2025-08-02/README.txt
@@ -0,0 +1,2 @@
+For Full Infographic, go to 
+https://app.displayr.com/Dashboard?id=339bcf9d-1200-4b0b-99f5-149bec18c0df#page=4c3e33d5-1715-478b-8b0e-62b8378f39ac
